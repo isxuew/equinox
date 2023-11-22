f47c18c1723437a2d521a59209bebba97aa7ab67
isxuew <isxuew@gmail.com>
2023-11-23 00:24:02 +0800

    update

---
+++ equinox-commons/equinox-common-api/src/main/java/com/github/isxuew/equinox/common/api/config/EquinoxCommonApiConfig.java
@@ -0,0 +1,10 @@
+package com.github.isxuew.equinox.common.api.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = "com.github.isxuew.equinox.common.api")
+public class EquinoxCommonApiConfig {
+
+}
