976f0385d5f15b2f77caa4bcf732fc5c886683ea
isxuew <isxuew@gmail.com>
2025-02-08 14:34:17 +0800

    update

--- equinox-commons/equinox-common-api/src/main/java/com/github/isxuew/equinox/common/api/util/ApiUtil.java
+++ equinox-commons/equinox-common-api/src/main/java/com/github/isxuew/equinox/common/api/util/ApiUtil.java
@@ -1,17 +1,38 @@
 package com.github.isxuew.equinox.common.api.util;
 
-import java.util.Objects;
+import com.github.isxuew.equinox.common.core.exception.CheckedException;
+import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.OK;
+import java.util.Objects;
+import java.util.Optional;
 
 public class ApiUtil {
 
-    public static boolean isOK(R response) {
-        return Objects.nonNull(response) && response.getCode() == OK.value();
+    public static <T> boolean isOK(R<T> response) {
+        return Objects.nonNull(response) && HttpStatus.valueOf(response.getCode()).is2xxSuccessful();
     }
 
-    public static boolean hasData(R response) {
-        return Objects.nonNull(response.getData());
+    public static <T> boolean checkOK(R<T> response) {
+        return checkOK(response, "响应体为空");
+    }
+
+    public static <T> boolean checkOK(R<T> response, String message) {
+        if (Objects.isNull(response)) {
+            throw new CheckedException(message);
+        }
+        if (HttpStatus.valueOf(response.getCode()).is2xxSuccessful()) {
+            return true;
+        }
+        throw new CheckedException("响应码：" + response.getCode() + "，" + response.getMessage());
+    }
+
+    public static <T> Optional<T> getData(R<T> response) {
+        return isOK(response) ? Optional.of(response.getData()) : Optional.empty();
+    }
+
+    public static <T> Optional<T> getData(R<T> response, String message) {
+        checkOK(response, message);
+        return Optional.ofNullable(response.getData());
     }
 
 }
